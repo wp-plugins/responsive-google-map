340c3ffb8a6d303c29c6e45f4a037950b37f5ecc
jogesh_pi <jogesh_pi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 06:13:26 +0000

    #New - Boilerplate implementation - https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate #Fix - Fixed Content overlap bug
    git-svn-id: https://plugins.svn.wordpress.org/responsive-google-map/tags/2.1@1070268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/rgm-deactivator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Fired during Plugin Deactivation
+ * 
+ * @link http://webomnizz.com
+ * @since 2.1
+ */
+class RGM_Deactivator {
+    
+    
+        public static function deactivate() {
+            
+        }    
+}
---
+++ uninstall.php
@@ -0,0 +1,6 @@
+<?php
+
+// If not called form wordpress then exit.
+if( ! defined('WP_UNINSTALL_PLUGIN') ) {
+    exit;
+}
